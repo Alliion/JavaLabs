fe2d935f75e92aee1ec410f5e4b0946668d4e2f0
alion <kruzina.alina@gmail.com>
2020-10-30 03:37:15 +0300

    +lab5

---
+++ src/lab5/Task5.java
@@ -0,0 +1,16 @@
+package lab5;
+
+public class Task5 {
+    static int sum(int a) {
+        if (a < 10) {
+            return a;
+        } else {
+            return a % 10 + sum(a / 10);
+        }
+    }
+
+    public static void main(String[] args){
+        int N = 123;
+        System.out.println(sum(N));
+    }
+}
